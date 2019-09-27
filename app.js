26eadbfd4687e7406e8b456b178943213b28de23
JeffAbney <jeffmabney@gmail.com>
2019-09-27 11:33:02 -0500

    move cors permissions up

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
-var app = express();
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 
+const app = express();
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", '*');
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+	next();
+  });
+  
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+
+
+
 var connection = mysql.createConnection({
 	host: 'remotemysql.com',
 	user: 'ZogkJE9gUn',
